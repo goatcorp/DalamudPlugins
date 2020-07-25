776f4d6b010b7ebb9d149682bdbc04c87f2d9378
goaaats <16760685+goaaats@users.noreply.github.com>
2020-07-25 10:54:24 +0200

    Update Make-PluginMaster.ps1

--- Make-PluginMaster.ps1
+++ Make-PluginMaster.ps1
@@ -1,5 +1,5 @@
 $output = New-Object Collections.Generic.List[object]
-$notInclude = "ChatCoordinates", "dhfnf", "XIVStats", "MarketBoardPlugin";
+$notInclude = "ChatCoordinates", "dhfnf", "XIVStats", "SHDHJFK";
 
 Get-ChildItem -Path plugins -File -Recurse -Include *.json |
 Foreach-Object {
