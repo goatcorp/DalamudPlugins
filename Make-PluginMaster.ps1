dd54449a5812f87a3ec21fe0043124f92a0783f0
goat <16760685+goaaats@users.noreply.github.com>
2020-07-30 20:10:57 +0200

    add plugin website

--- Make-PluginMaster.ps1
+++ Make-PluginMaster.ps1
@@ -1,6 +1,8 @@
 $output = New-Object Collections.Generic.List[object]
 $notInclude = "ChatCoordinates", "dhfnf", "XIVStats", "SHDHJFK";
 
+$table = ""
+
 Get-ChildItem -Path plugins -File -Recurse -Include *.json |
 Foreach-Object {
     $content = Get-Content $_.FullName | ConvertFrom-Json
@@ -11,6 +13,7 @@ Foreach-Object {
     else
     {
     	$content | add-member -Name "IsHide" -value "False" -MemberType NoteProperty
+    	$table = $table + "| " + $content.Author + " | " + $content.Name + " | " + $content.Description + " |`n"
     }
 
     $output.Add($content)
@@ -20,3 +23,7 @@ $outputStr = $output | ConvertTo-Json
 echo $outputStr
 
 Out-File -FilePath .\pluginmaster.json -InputObject $outputStr
+
+$template = Get-Content -Path mdtemplate.txt
+$template = $template + $table
+Out-File -FilePath .\plugins.md -InputObject $template
