94ddfb3949e8c0579f066e0e1dc20716d48041fb
goaaats <16760685+goaaats@users.noreply.github.com>
2020-12-08 17:36:56 +0100

    Update Make-PluginMaster.ps1

--- Make-PluginMaster.ps1
+++ Make-PluginMaster.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
 $output = New-Object Collections.Generic.List[object]
-$notInclude = "GoodMemory", "SlideCast", "XIVStats", "TitleEdit", "VoidList", "Globetrotter", "Visibility", "SimpleTweaks";
+$notInclude = "GoodMemory", "SlideCast", "XIVStats", "TitleEdit", "VoidList", "Globetrotter", "Visibility", "SimpleTweaksPlugin";
 
 $counts = Get-Content "downloadcounts.json" | ConvertFrom-Json
 
