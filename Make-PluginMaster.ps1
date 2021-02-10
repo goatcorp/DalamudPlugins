7174b11602b956150b1d3fe5742754bf81f1695b
goaaats <16760685+goaaats@users.noreply.github.com>
2021-02-10 11:13:13 +0100

    Update Make-PluginMaster.ps1

--- Make-PluginMaster.ps1
+++ Make-PluginMaster.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
 $output = New-Object Collections.Generic.List[object]
-$notInclude = "PingPlugin", "sdafsaf", "XIVStats", "bffbbf", "VoidList", "asdfsad", "sdfgdfsg", "vrgnddgv";
+$notInclude = "PingPlugin", "VFXEditor", "XIVStats", "bffbbf", "VoidList", "asdfsad", "sdfgdfsg", "vrgnddgv";
 
 $counts = Get-Content "downloadcounts.json" | ConvertFrom-Json
 
