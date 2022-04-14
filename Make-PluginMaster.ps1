7a966e4ef82e2a50586b345952e107f8b76cc779
Robert Baker <reiichi001@gmail.com>
2022-04-13 21:49:41 -0700

    Make pluginmaster target api6
    Maybe this will fix regen?

--- Make-PluginMaster.ps1
+++ Make-PluginMaster.ps1
@@ -11,10 +11,10 @@ $pluginBlacklistUrl = "https://goatcorp.github.io/DalamudAssets/UIRes/bannedplug
 $wc = New-Object system.Net.WebClient
 $blackList = $wc.downloadString($pluginBlacklistUrl) | ConvertFrom-Json
 
-$dlTemplateInstall = "https://kamori.goats.dev/Plugin/Download/{0}?isUpdate=False&isTesting={1}&branch=api5"
-$dlTemplateUpdate = "https://raw.githubusercontent.com/goatcorp/DalamudPlugins/api5/{0}/{1}/latest.zip"
+$dlTemplateInstall = "https://kamori.goats.dev/Plugin/Download/{0}?isUpdate=False&isTesting={1}&branch=api6"
+$dlTemplateUpdate = "https://raw.githubusercontent.com/goatcorp/DalamudPlugins/api6/{0}/{1}/latest.zip"
 
-$apiLevel = 5
+$apiLevel = 6
 
 $thisPath = Get-Location
 
